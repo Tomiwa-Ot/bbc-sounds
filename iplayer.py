43b98be7ac3768c068b8f3df972719d242f3ed64
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-03-11 18:54:12 +0100

    Update iplayer.py

--- iplayer.py
+++ iplayer.py
@@ -8,8 +8,9 @@ import argparse
 
 URL = ""
 ID =  ""
-MIME_TYPE = ""
+PROG_TYPE = ""
 FILE_META_DATA = {}
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -19,19 +20,19 @@ def parse_args():
     )
     parser.add_argument(
         "--url", type=str, required=False,
-        help="url of the content"
+        help="url of the programme"
     )
     parser.add_argument(
         "--id", type=str, required=False,
-        help="id of the content"
+        help="id of the programme"
     )
     parser.add_argument(
         "--type", type=str, required=True,
-        help="type of content (audio/video)"
+        help="type of programme (audio/video)"
     )
     args = parser.parse_args()
     if not args.type == "audio" or not args.type == "video":
-        print("[+] Invalid MIME type")
+        print("[+] Invalid programme type")
         exit(1)
     if args.url is not None and args.id is not None:
         print("[+] Specify either --url or --id")
@@ -43,7 +44,7 @@ def parse_args():
         URL = args.url
     if args.id is not None and args.url is None:
         ID = args.id
-    MIME_TYPE = args.type
+    PROG_TYPE = args.type
 
 def get_audio_meta_data():
     req = "https://bbc.co.uk/programmes/"
@@ -54,13 +55,15 @@ def get_audio_meta_data():
         req = f"{req}{ID}.json"
     resp = requests.get(req)
     FILE_META_DATA = resp.json
-    
+    req = f"https://open.live.bbc.co.uk/mediaselector/6/version/2.0/mediaset/pc/vpid/{FILE_META_DATA['versions'][0]['pid']}"
+    resp = requests.get(req)
+    FILE_META_DATA.update(resp.json)
 
 def get_video_meta_data():
     pass
 
 def download_thumbnail():
-    if MIME_TYPE == "audio":
+    if PROG_TYPE == "audio":
         req = f"https:{FILE_META_DATA['holdingImage']}"
         resp = requests.get(req)
         with open(f"iplayer/{ID}.{FILE_META_DATA['holdingImage'].split('.')[-1]}", "rb") as f:
@@ -74,19 +77,24 @@ def download_file(mpd_link):
 
 def is_ffmpeg_installed():
     if platform.system() == "Windows":
-        pass
+        if b"'ffmpeg' is not recognized as an internal or external command" in subprocess.Popen(
+            "ffmpeg", shell=True, stderr=subprocess.PIPE).stderr.read():
+            print("[+] ffmpeg is not installed")
+            exit(1)
     else:
-        pass
+        if b"ffmpeg: not found" in subprocess.Popen(
+            "ffmpeg", shell=True, stderr=subprocess.PIPE).stderr.read():
+            print("[+] ffmpeg is not installed")
+            exit(1)
 
 def main():
     parse_args()
-    if not is_ffmpeg_installed():
-        print("[+] ffmpeg is not installed")
-        exit(1)
-    if MIME_TYPE == "audio":
+    is_ffmpeg_installed()
+    if PROG_TYPE == "audio":
         get_audio_meta_data()
     else:
         get_video_meta_data()
+    download_file()
 
 if __name__ == "__main__":
     main()
