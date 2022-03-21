7b0b4f90d4337636c7a1e82761eb79b5d21f0032
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-03-21 20:45:31 +0100

    made some changes

--- sounds.py
+++ sounds.py
@@ -4,7 +4,7 @@
 # Description: Download shows from BBC Sounds
 # License: GPLv3 (see LICENSE.txt)
 
-from future import print_function
+from __future__ import print_function
 from colorama import Fore
 import subprocess
 import platform
@@ -14,6 +14,7 @@ import shutil
 
 URL = ""
 ID =  ""
+QUALITY = ""
 FILE_META_DATA = {}
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"  \
             "AppleWebKit/537.36 (KHTML, like Gecko)" \
@@ -25,7 +26,7 @@ HEADERS = {
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        prog="iplayer",
+        prog="BBC Sounds",
         description="Description: downloads radio programmes from BBC Sounds",
         formatter_class=argparse.RawDescriptionHelpFormatter
     )
@@ -37,7 +38,18 @@ def parse_args():
         "--id", type=str, required=False,
         help="id of the programme"
     )
+    parser.add_argument(
+        "--quality", type=str, required=True,
+        help="quality of the download"
+    )
     args = parser.parse_args()
+    if args.quality is None: 
+        print("[+] Specify quality --quality (worst/best)")
+        exit(1)
+    if args.quality != "worst" or args.quality != "best":
+        print(Fore.RED + "[!] Invalid quality")
+        exit(1)
+    QUALITY = args.quality
     if args.url is not None and args.id is not None:
         print("[+] Specify either --url or --id")
         exit(1)
@@ -51,53 +63,60 @@ def parse_args():
 
 
 def get_audio_meta_data():
-    req = "https://bbc.co.uk/programmes/"
     if URL is not None:
         ID = URL.split("/")[-1]
-        req = f"{req}{ID}.json"
-    else:
-        req = f"{req}{ID}.json"
-    # check if request fails
-    resp = requests.get(req, headers=HEADERS)
-    FILE_META_DATA = resp.json
+    req_urls = [
+        f"https://bbc.co.uk/programmes/{ID}.json",
+        f"https://www.bbc.co.uk/programmes/{ID}/playlist.json",
+    ]
+    for url in req_urls:
+        resp = requests.get(url, headers=HEADERS)
+        if resp.status_code != 200:
+            print(Fore.RED + f"[!] GET request to {resp.url} failed: {resp.status_code}")
+            exit(1)
+        FILE_META_DATA.update(resp.json)
     req = f"https://open.live.bbc.co.uk/mediaselector/6/version/2.0/mediaset/pc/vpid/{FILE_META_DATA['versions'][0]['pid']}"
     resp = requests.get(req, headers=HEADERS)
+    if resp.status_code != 200:
+        print(Fore.RED + f"[!] GET request to {resp.url} failed: {resp.status_code}")
+        exit(1)
     FILE_META_DATA.update(resp.json)
+    
 
 
 def download_thumbnail():
     print("[+] Downloading thumbnail ...")
     req = f"https:{FILE_META_DATA['holdingImage']}"
     resp = requests.get(req, headers=HEADERS, stream=True)
-    with open(f"iplayer/{ID}.{FILE_META_DATA['holdingImage'].split('.')[-1]}", "wb") as f:
+    with open(f"{ID}.{FILE_META_DATA['holdingImage'].split('.')[-1]}", "wb") as f:
         shutil.copyfileobj(resp.raw, f)
         print(Fore.GREEN + f"[+] Thumbnail saved {ID}.{FILE_META_DATA['holdingImage'].split('.')[-1]}")
 
 
 def download_file(mpd_link):
-    # option of best and worse
-    cmd = f"streamlink {mpd_link} -o {FILE_META_DATA['']}.m4a"
+    cmd = f"streamlink {mpd_link} {QUALITY} -o {FILE_META_DATA['title']}.m4a"
     print(subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read())
     print(Fore.GREEN + "[+] Download complete")
 
 
 def is_streamlink_installed():
     if platform.system() == "Windows":
-        if b"'ffmpeg' is not recognized as an internal or external command" in subprocess.Popen(
+        if b"'streamlink' is not recognized as an internal or external command" in subprocess.Popen(
             "streamlink", shell=True, stderr=subprocess.PIPE).stderr.read():
-            print(Fore.RED + "[!] ffmpeg is not installed")
+            print(Fore.RED + "[!] streamlink is not installed")
             exit(1)
     else:
-        if b"ffmpeg: not found" in subprocess.Popen(
-            "ffmpeg", shell=True, stderr=subprocess.PIPE).stderr.read():
-            print(Fore.RED + "[!] ffmpeg is not installed")
+        if b"streamlink: not found" in subprocess.Popen(
+            "streamlink", shell=True, stderr=subprocess.PIPE).stderr.read():
+            print(Fore.RED + "[!] streamlink is not installed")
             exit(1)
 
 
 def main():
     parse_args()
-    is_streamkink_installed()
+    is_streamlink_installed()
     get_audio_meta_data()
+    download_thumbnail()
     download_file()
 
 
