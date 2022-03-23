05f3fbb7586439c0e34d1c0e4e7b60e8d0fe3883
Olorunfemi-Ojo Tomiwa <ot.server1@outlook.com>
2022-03-23 20:55:41 +0100

    fixed bugs

--- sounds.py
+++ sounds.py
@@ -12,19 +12,27 @@ import requests
 import argparse
 import shutil
 
-URL = ""
-ID =  ""
-QUALITY = ""
-FILE_META_DATA = {}
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"  \
-            "AppleWebKit/537.36 (KHTML, like Gecko)" \
-            "Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393"
+url = ""
+id =  ""
+quality = ""
+file_meta_data = []
+
 HEADERS = {
-    "User-Agent" : USER_AGENT,
-    "Accept-Encoding" : "gzip,deflate"
+    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
+    "Accept-Encoding" : "gzip,deflate",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "cross-site",
+    "Cache-Control": "max-age=0"
 }
 
 def parse_args():
+    global url, id, quality
     parser = argparse.ArgumentParser(
         prog="BBC Sounds",
         description="Description: downloads radio programmes from BBC Sounds",
@@ -46,10 +54,10 @@ def parse_args():
     if args.quality is None: 
         print("[+] Specify quality --quality (worst/best)")
         exit(1)
-    if args.quality != "worst" or args.quality != "best":
+    if not (args.quality == "worst" or args.quality == "best"):
         print(Fore.RED + "[!] Invalid quality")
         exit(1)
-    QUALITY = args.quality
+    quality = args.quality
     if args.url is not None and args.id is not None:
         print("[+] Specify either --url or --id")
         exit(1)
@@ -57,44 +65,47 @@ def parse_args():
         print("[+] Specify either --url or --id")
         exit(1)
     if args.url is not None and args.id is None:
-        URL = args.url
+        url = args.url
     if args.id is not None and args.url is None:
-        ID = args.id
+        id = args.id
 
 
 def get_audio_meta_data():
-    if URL is not None:
-        ID = URL.split("/")[-1]
+    global url, id, quality, file_meta_data
+    if len(url) > 1:
+        id = url.split("/")[-1]
     req_urls = [
-        f"https://bbc.co.uk/programmes/{ID}.json",
-        f"https://www.bbc.co.uk/programmes/{ID}/playlist.json",
+        f"https://bbc.co.uk/programmes/{id}.json",
+        f"https://www.bbc.co.uk/programmes/{id}/playlist.json",
     ]
     for url in req_urls:
         resp = requests.get(url, headers=HEADERS)
         if resp.status_code != 200:
             print(Fore.RED + f"[!] GET request to {resp.url} failed: {resp.status_code}")
             exit(1)
-        FILE_META_DATA.update(resp.json)
-    req = f"https://open.live.bbc.co.uk/mediaselector/6/version/2.0/mediaset/pc/vpid/{FILE_META_DATA['versions'][0]['pid']}"
+        file_meta_data.append(resp.json())
+    req = f"https://open.live.bbc.co.uk/mediaselector/6/version/2.0/mediaset/pc/vpid/{file_meta_data[0]['programme']['versions'][0]['pid']}"
     resp = requests.get(req, headers=HEADERS)
-    if resp.status_code != 200:
-        print(Fore.RED + f"[!] GET request to {resp.url} failed: {resp.status_code}")
-        exit(1)
-    FILE_META_DATA.update(resp.json)
+    # if resp.status_code != 200:
+    #     print(Fore.RED + f"[!] GET request to {resp.url} failed: {resp.status_code}")
+    #     exit(1)
+    # file_meta_data.append(resp.json())
     
 
 
 def download_thumbnail():
+    global id, file_meta_data
     print("[+] Downloading thumbnail ...")
-    req = f"https:{FILE_META_DATA['holdingImage']}"
+    req = f"https:{file_meta_data[1]['holdingImage']}"
     resp = requests.get(req, headers=HEADERS, stream=True)
-    with open(f"{ID}.{FILE_META_DATA['holdingImage'].split('.')[-1]}", "wb") as f:
+    with open(f"{id}.{file_meta_data[1]['holdingImage'].split('.')[-1]}", "wb") as f:
         shutil.copyfileobj(resp.raw, f)
-        print(Fore.GREEN + f"[+] Thumbnail saved {ID}.{FILE_META_DATA['holdingImage'].split('.')[-1]}")
+        print(Fore.GREEN + f"[+] Thumbnail saved {id}.{file_meta_data[1]['holdingImage'].split('.')[-1]}")
 
 
 def download_file(mpd_link):
-    cmd = f"streamlink {mpd_link} {QUALITY} -o {FILE_META_DATA['title']}.m4a"
+    global quality, file_meta_data
+    cmd = f"streamlink {mpd_link} {quality} -o {file_meta_data[0]['title']}.m4a"
     print(subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read())
     print(Fore.GREEN + "[+] Download complete")
 
